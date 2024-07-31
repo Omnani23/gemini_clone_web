9453ac3c2b1544a5e7f82fc92e46d8b5c9c2dfc5
Omnani23 <nakkaomprakash98@gmail.com>
2024-07-31 19:57:18 +0530

    Refactor layout.tsx for better readability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,13 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import ContextProvider from "@/context/ContextProvider";
 import { ReactNode } from "react";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
-<<<<<<< HEAD
-export const metadata = {
-  title: "OmGPT",
-  description: "Created by Om Prakash",
-=======
 export const metadata: Metadata = {
   title: "OmGPT",
   description: "created by Omprakash",
->>>>>>> 82eb9d0716173056c7f3ccf7321d1e3ebb63a1b6
 };
 
 interface RootLayoutProps {
